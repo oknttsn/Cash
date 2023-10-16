233a19b7cce5d8fc2ce0cd0b16bd603f2487b338
oknttsn <okan.ttsn@gmail.com>
2023-10-16 19:03:30 +0300

    wip

---
+++ Cash.Web/Controllers/ClientAccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cash.Web.Controllers
+{
+    public class ClientAccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Cash.Web/Controllers/CustomerLayoutController.cs
+++ Cash.Web/Controllers/CustomerLayoutController.cs
@@ -2,7 +2,7 @@
 
 namespace Cash.Web.Controllers;
 
-public class MyProfileController : Controller
+public class CustomerLayoutController : Controller
 {
     public IActionResult Index()
     {
